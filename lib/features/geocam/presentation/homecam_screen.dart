cdadf57132747e44946eb8aa163b1a1b3dc8c0dd
reihanbay <bung.reihan28@gmail.com>
2025-04-15 21:40:13 +0700

    feat: slicing homeCameraLocation

--- lib/features/geocam/presentation/homecam_screen.dart
+++ lib/features/geocam/presentation/homecam_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:location/location.dart';
 
 class HomecamScreen extends StatefulWidget {
   const HomecamScreen({super.key});
@@ -8,8 +10,168 @@ class HomecamScreen extends StatefulWidget {
 }
 
 class _HomecamScreenState extends State<HomecamScreen> {
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            children: [
+               Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "GeoCam News",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+            
+              ContainerCapture(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerCapture extends StatelessWidget {
+  const ContainerCapture({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xFF8F8F8F).withOpacity(0.15),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1510265236892-329bfd7de7a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3RyZWV0fGVufDB8fDB8fHww',
+                  height: 200,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Theme.of(context).colorScheme.surface.withValues(alpha: 0.9), // semi-transparent black
+                      Colors.transparent,
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                  bottom: 16,
+                  left: 16,
+                  right: 16,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Latitude",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                              )),
+                          Text("Tidak Terdeteksi",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Longitude",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                              )),
+                          Text("Tidak Terdeteksi",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ],
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Theme.of(context).colorScheme.primary),
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    "Reset",
+                    style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 6),
+              FloatingActionButton.small(
+                onPressed: () {},
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                child: SvgPicture.asset("assets/camera.svg"),
+              ),
+              const SizedBox(width: 6),
+              Expanded(
+                child: ElevatedButton.icon(
+                  onPressed: () {
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  label: Text("Simpan Data", style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
