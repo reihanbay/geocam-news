1ece86b892e3568888b102598b07c2fd8144fc14
reihanbay <bung.reihan28@gmail.com>
2025-04-15 12:58:32 +0700

    Setup Base Route

---
+++ lib/features/geocam/presentation/homecam_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomecamScreen extends StatefulWidget {
+  const HomecamScreen({super.key});
+
+  @override
+  State<HomecamScreen> createState() => _HomecamScreenState();
+}
+
+class _HomecamScreenState extends State<HomecamScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/main_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:geocam_news/features/geocam/presentation/homecam_screen.dart';
+import 'package:geocam_news/features/news/presentation/news_screen.dart';
+import 'package:geocam_news/shared/bottomnav_provider.dart';
+import 'package:provider/provider.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        height: 74,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        onDestinationSelected: (value) {
+          context.read<BottomNavProvider>().setIndexBottomNav = value;
+        },
+        selectedIndex: context.watch<BottomNavProvider>().indexBottomNav,
+        destinations: [
+          NavigationDestination(
+            icon: SvgPicture.asset("assets/location.svg"), 
+            selectedIcon: SvgPicture.asset('assets/location_on.svg'),
+            label: "Home"),
+          NavigationDestination(
+            icon: SvgPicture.asset("assets/news.svg"), 
+            selectedIcon: SvgPicture.asset('assets/news_on.svg'),
+            label: "News")
+      ]),
+      body: Consumer<BottomNavProvider>(builder: (context, value, child) {
+        return <Widget>[
+          const HomecamScreen(),
+          const NewsScreen()
+        ][value.indexBottomNav];
+      }),
+    );
+  }
+}
